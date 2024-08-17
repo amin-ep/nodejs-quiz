1ae089a2ea5567d6be0afc9a5caf7d3cd08536d6
Amin <amincode24@gmail.com>
2024-08-17 18:53:48 +0330

    user routes

---
+++ src/api/v1/routes/userRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import UserController from '../controllers/userController.js';
+import ProtectMiddlewares from '../middlewares/protectMiddlewares';
+
+const router = Router();
+
+const { getAllUsers, getUserById } = new UserController();
+const { protect, restrictTo } = new ProtectMiddlewares();
+
+router.use(protect);
+
+router.route('/').get(restrictTo('admin'), getAllUsers);
+router.route('/:id').get(restrictTo('admin', 'teacher'), getUserById);
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express, { Express } from 'express';
 import HttpError from './utils/httpError.js';
 import globalErrorHandler from './api/v1/handlers/errorHandler.js';
 import authRouterV1 from './api/v1/routes/authRoutes.js';
-import quizRouter from './api/v1/routes/quizRoutes.js';
-import questionRouter from './api/v1/routes/questionRoutes.js';
+import quizRouterV1 from './api/v1/routes/quizRoutes.js';
+import questionRouterV1 from './api/v1/routes/questionRoutes.js';
+import userRouterV1 from './api/v1/routes/userRoutes.js';
+
 const app: Express = express();
 
 app.use(express.json());
@@ -12,8 +14,9 @@ app.use(express.json());
 
 // V1
 app.use('/api/v1/auth', authRouterV1);
-app.use('/api/v1/quiz', quizRouter);
-app.use('/api/v1/question', questionRouter);
+app.use('/api/v1/quiz', quizRouterV1);
+app.use('/api/v1/question', questionRouterV1);
+app.use('/api/v1/users', userRouterV1);
 
 app.all('*', function (req, res, next) {
   return next(
